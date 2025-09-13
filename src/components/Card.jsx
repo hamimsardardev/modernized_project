e0e2c3c68b9fc7b6b2dee455e78219e74f278c65
hamimsardardev <hamimsardar118677@gmail.com>
2025-09-13 12:58:38 +0600

    responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -58,24 +58,30 @@ export default function Card() {
     <section className='bg-[url("/images/reviewbg.png")] bg-cover bg-center bg-no-repeat w-full py-15'>
       <div className="container mx-auto px-5">
         <div className="text-center">
-            <h1 className="font-semibold relative text-[48px] text-[#012645] leading-[168%] mb-[76px] after:content-[''] after:absolute after:bottom-[-5px] after:left-[50%] after: after:bg-[#FFB000] after:w-0 hover:after:w-[58px] after:h-[5px] after:transform after:translate-x-[-50%] after:translate-y-[-50%]">
-              What We Offer
-            </h1>
-          </div>
+          <h1 className="font-semibold relative text-[48px] text-[#012645] leading-[168%] mb-[76px] after:content-[''] after:absolute after:bottom-[-5px] after:left-[50%] after: after:bg-[#FFB000] after:w-0 hover:after:w-[58px] after:h-[5px] after:transform after:translate-x-[-50%] after:translate-y-[-50%]">
+            What We Offer
+          </h1>
+        </div>
 
         <div className="mt-20">
           <Slider ref={sliderRef} {...settings}>
             <div
-              className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[24px] 
-                          pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF]"
+              className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[24px]
+              pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF]
+              sm:ml-0 sm:pt-[16px] sm:pb-[24px] sm:pr-[20px] sm:pl-[16px]
+              md:pt-[24px] md:pb-[35px] md:pr-[29px] md:pl-[25px]"
             >
-              <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center w-24 h-24">
-                <img src={offerimage1} alt="offerimage1" />
+              <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center lg:w-24 lg:h-24 w-15 h-15">
+                <img
+                  src={offerimage1}
+                  alt="offerimage1"
+                  className="w-8 h-8 lg:w-15 lg:h-15"
+                />
               </div>
-              <h2 className="font-bold text-[20px] text-[#000000] leading-[180%] mt-[12px] pb-3 hover:text-[#012645]">
+              <h2 className="font-bold lg:text-[20px] text-[18px] text-[#000000] leading-[180%] lg:mt-[12px] mt-[8px] pb-3 hover:text-[#012645]">
                 App Development
               </h2>
-              <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645]">
+              <p className="w-auto lg:font-medium font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645]">
                 We develop complex, high-quality iOS, Android, and Windows apps
                 - and we can work in almost any niche. When designing apps, we
                 aim for memorability and stability, crafting a foundation for
@@ -85,12 +91,12 @@ export default function Card() {
               </p>
               <button
                 onClick={handleReadToggle1}
-                className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] pt-9 cursor-pointer hover:text-red-500"
+                className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
               >
                 {!viewdetails1 && "View Details"}
               </button>
               {viewdetails1 && (
-                <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] ">
+                <p className="w-auto font-medium lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] ">
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   Fugit, accusamus? Nihil alias ab distinctio tempora qui sint
                   ea minima quibusdam!
@@ -98,20 +104,28 @@ export default function Card() {
               )}
               <button
                 onClick={handleReadToggle1}
-                className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
+                className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
               >
                 {viewdetails1 && "View Less"}
               </button>
             </div>
             <div>
-              <div className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[38px] pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF]">
-                <div className="rounded-full border-[3px] border-[#FFB000] flex items-center justify-center w-28 h-28">
-                  <img src={offerimage2} alt="offerimage2" />
+              <div
+                className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[24px]
+              pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF] sm:ml-0 sm:pt-[16px] sm:pb-[24px] sm:pr-[20px] 
+              sm:pl-[16px] md:pt-[24px] md:pb-[35px] md:pr-[29px] md:pl-[25px]"
+              >
+                <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center lg:w-28 lg:h-28 w-16 h-16">
+                  <img
+                    src={offerimage2}
+                    alt="offerimage2"
+                    className="w-10 h-10 lg:w-15 lg:h-15"
+                  />
                 </div>
-                <h2 className="font-bold text-[20px] text-[#000000] leading-[180%] mt-[12px] pb-3 hover:text-[#012645]">
+                <h2 className="font-bold lg:text-[20px] text-[18px] text-[#000000] leading-[180%] lg:mt-[12px] mt-[8px] pb-3 hover:text-[#012645]">
                   Digital Marketing
                 </h2>
-                <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] pb-5 group-hover:text-[#012645]">
+                <p className="w-auto lg:font-medium font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] pb-5 group-hover:text-[#012645]">
                   Having trouble spreading the word about your product? Let us
                   formulate a strategy to reach your audience through a mixture
                   of SEO, ads, and a solid content strategy. We’ll perform
@@ -121,12 +135,12 @@ export default function Card() {
                 </p>
                 <button
                   onClick={handleReadToggle2}
-                  className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] pt-5 cursor-pointer hover:text-red-500"
+                  className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
                 >
                   {!viewdetails2 && "View Details"}
                 </button>
                 {viewdetails2 && (
-                  <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] ">
+                  <p className="w-auto font-medium lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645]">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Fugit, accusamus? Nihil alias ab distinctio tempora qui sint
                     ea minima quibusdam!
@@ -134,55 +148,70 @@ export default function Card() {
                 )}
                 <button
                   onClick={handleReadToggle2}
-                  className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
+                  className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
                 >
                   {viewdetails2 && "View Less"}
                 </button>
               </div>
             </div>
-            <div className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl/20 pt-[38px] pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF]">
-              <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center w-28 h-28">
-                <img src={offerimage3} alt="offerimage3" />
-              </div>
-              <h2 className="font-bold text-[20px] text-[#000000] leading-[180%] mt-[12px] pb-3 hover:text-[#012645]">
-                Web Planning & Development
-              </h2>
-              <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] pb-5 group-hover:text-[#012645]">
-                Looking to establish or improve your business’s online presence?
-                Let us craft a website that compliments and propagates your
-                business – designing a website that fits your niche and your
-                tone while standing out from your competitors Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Voluptas, maxime..
-              </p>
-              <button
-                onClick={handleReadToggle3}
-                className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] pt-5 cursor-pointer hover:text-red-500"
+            <div>
+              <div
+                className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[24px]
+              pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF] sm:ml-0 sm:pt-[16px] sm:pb-[24px] sm:pr-[20px] 
+              sm:pl-[16px] md:pt-[24px] md:pb-[35px] md:pr-[29px] md:pl-[25px]"
               >
-                {!viewdetails3 && "View Details"}
-              </button>
-              {viewdetails3 && (
-                <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] group-holeadertext-[#012645] ">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Fugit, accusamus? Nihil alias ab distinctio tempora qui sint
-                  ea minima quibusdam!
+                <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center lg:w-28 lg:h-28 w-16 h-16">
+                  <img
+                    src={offerimage3}
+                    alt="offerimage3"
+                    className="w-10 h-10 lg:w-15 lg:h-15"
+                  />
+                </div>
+                <h2 className="font-bold lg:text-[20px] text-[18px] text-[#000000] leading-[180%] lg:mt-[12px] mt-[8px] pb-3 hover:text-[#012645]">
+                  Web Planning & Development
+                </h2>
+                <p className="w-auto lg:font-medium font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] pb-5 group-hover:text-[#012645]">
+                  Looking to establish or improve your business’s online
+                  presence? Let us craft a website that compliments and
+                  propagates your business – designing a website that fits your
+                  niche and your tone while standing out from your competitors
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptas, maxime..
                 </p>
-              )}
-              <button
-                onClick={handleReadToggle3}
-                className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
-              >
-                {viewdetails3 && "View Less"}
-              </button>
+                <button
+                  onClick={handleReadToggle3}
+                  className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
+                >
+                  {!viewdetails3 && "View Details"}
+                </button>
+                {viewdetails3 && (
+                  <p className="w-auto font-medium lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645]">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Fugit, accusamus? Nihil alias ab distinctio tempora qui sint
+                    ea minima quibusdam!
+                  </p>
+                )}
+                <button
+                  onClick={handleReadToggle3}
+                  className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
+                >
+                  {viewdetails3 && "View Less"}
+                </button>
+              </div>
             </div>
             <div>
-              <div className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[38px] pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF]">
-                <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center w-28 h-28">
-                  <LuBriefcaseBusiness className="h-[51px] w-[51px]" />
+              <div
+                className="group ml-3 bg-[#FFB000] border border-gray-300 rounded-[14px] shadow-xl pt-[24px]
+              pb-[35px] pr-[29px] pl-[25px] hover:bg-[#FFFFFF] sm:ml-0 sm:pt-[16px] sm:pb-[24px] sm:pr-[20px] 
+              sm:pl-[16px] md:pt-[24px] md:pb-[35px] md:pr-[29px] md:pl-[25px]"
+              >
+                <div className="rounded-full border-[3px] border-[#7F5800] flex items-center justify-center lg:w-28 lg:h-28 w-16 h-16">
+                  <LuBriefcaseBusiness className="lg:h-[51px] lg:w-[51px] h-10 w-10 lg:w-15 lg:h-15" />
                 </div>
-                <h2 className="font-bold text-[20px] text-[#000000] leading-[180%] mt-[12px] pb-3 hover:text-[#012645]">
+                <h2 className="font-bold lg:text-[20px] text-[18px] text-[#000000] leading-[180%] lg:mt-[12px] mt-[8px] pb-3 hover:text-[#012645]">
                   Business Solutions
                 </h2>
-                <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] pb-5 group-hover:text-[#012645]">
+                <p className="w-auto lg:font-medium font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] pb-5 group-hover:text-[#012645]">
                   Looking to establish or improve your business’s online
                   presence? Let us craft a website that compliments and
                   propagates your business – designing a website that fits your
@@ -192,12 +221,12 @@ export default function Card() {
                 </p>
                 <button
                   onClick={handleReadToggle4}
-                  className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] pt-5 cursor-pointer hover:text-red-500"
+                  className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
                 >
                   {!viewdetails4 && "View Details"}
                 </button>
                 {viewdetails4 && (
-                  <p className="w-auto font-medium text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] ">
+                  <p className="w-auto font-medium lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645]">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                     Fugit, accusamus? Nihil alias ab distinctio tempora qui sint
                     ea minima quibusdam!
@@ -205,7 +234,7 @@ export default function Card() {
                 )}
                 <button
                   onClick={handleReadToggle4}
-                  className="font-semibold text-[18px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
+                  className="lg:font-semibold font-normal lg:text-[18px] text-[16px] text-[#FFFFFF] leading-[168%] group-hover:text-[#012645] cursor-pointer hover:text-red-500"
                 >
                   {viewdetails4 && "View Less"}
                 </button>
